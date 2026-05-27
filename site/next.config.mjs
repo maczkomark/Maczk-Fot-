4d36d40ec9c1869edde4c5ba4352885e730635fc
Maci <137713401+maczkomark@users.noreply.github.com>
2026-05-27 22:52:49 +0200

    cl

--- site/next.config.mjs
+++ site/next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   images: {
+    unoptimized: true,
     formats: ["image/avif", "image/webp"],
   },
+  trailingSlash: true,
 };
 
 export default nextConfig;
